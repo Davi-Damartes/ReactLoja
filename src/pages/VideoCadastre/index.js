2dfc887787b2099fc361e495494e3d30abe8b864
Davi Lima <davidamartesgit@Gmail.com>
2024-08-13 18:24:22 -0300

    implementa a estrutura da página para cadastro de vídeos.

---
+++ src/pages/VideoCadastre/index.js
@@ -0,0 +1,19 @@
+import Container from "../../components/Container/Index";
+import Footer from "../../components/Footer/Index";
+import Form from "../../components/Form/Index";
+import Header from "../../components/Header/Index";
+
+function VideoCadastre (){
+        return(
+            <>
+                <Header />
+                <Container > 
+                    <Form />
+                </Container>
+                <Footer/>
+            </>
+    );
+}
+
+export default VideoCadastre;
+
